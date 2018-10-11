9c9364d3e6b31ee46d8cd913855662882ab7db94
MatthewDodi <matthew.dodi@gmail.com>
2018-10-10 21:51:41 -0700

    Added logic to check if game is Won

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -11,6 +11,10 @@ const Grid = styled.div`
 
 const Square = styled.div`
     border: 3px solid black;
+    font-size: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 class Board extends React.Component {
@@ -20,16 +24,37 @@ class Board extends React.Component {
                 {id: 7, text: ""}, {id: 8, text: ""}, {id: 9, text: ""}]
     }
 
+    checkVictory = () => {
+        if ((this.state.tiles[0].text === "X" && this.state.tiles[1].text === "X" && this.state.tiles[2].text === "X") || (this.state.tiles[0].text === "O" && this.state.tiles[1].text === "O" && this.state.tiles[2].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[3].text === "X" && this.state.tiles[4].text === "X" && this.state.tiles[5].text === "X") || (this.state.tiles[3].text === "O" && this.state.tiles[4].text === "O" && this.state.tiles[5].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[6].text === "X" && this.state.tiles[7].text === "X" && this.state.tiles[8].text === "X") || (this.state.tiles[6].text === "O" && this.state.tiles[7].text === "O" && this.state.tiles[8].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[0].text === "X" && this.state.tiles[3].text === "X" && this.state.tiles[6].text === "X") || (this.state.tiles[0].text === "O" && this.state.tiles[3].text === "O" && this.state.tiles[6].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[1].text === "X" && this.state.tiles[4].text === "X" && this.state.tiles[7].text === "X") || (this.state.tiles[1].text === "O" && this.state.tiles[4].text === "O" && this.state.tiles[7].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[2].text === "X" && this.state.tiles[5].text === "X" && this.state.tiles[8].text === "X") || (this.state.tiles[2].text === "O" && this.state.tiles[5].text === "O" && this.state.tiles[8].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[0].text === "X" && this.state.tiles[4].text === "X" && this.state.tiles[8].text === "X") || (this.state.tiles[0].text === "O" && this.state.tiles[4].text === "O" && this.state.tiles[8].text === "O")) {
+            console.log("YOU WIN");
+        } else if ((this.state.tiles[2].text === "X" && this.state.tiles[4].text === "X" && this.state.tiles[6].text === "X") || (this.state.tiles[2].text === "O" && this.state.tiles[4].text === "O" && this.state.tiles[6].text === "O")) {
+            console.log("YOU WIN");
+        }
+    }
+
     clickHandler = (event) => {
         const key = +event.target.dataset.key;
         const tiles = [...this.state.tiles];
         tiles.forEach(tile => {
             if (tile.id === key) {
-                tile.text = "X";
+                tile.text = "O";
             }
         })
 
         this.setState({ tiles: tiles})
+        this.checkVictory();
     }
 
     render() {
